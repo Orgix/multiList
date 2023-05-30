ce6eba2368dfe3bdc5c0694ddb8f0683a1c4451c
Nick <tnikos28@gmail.com>
2023-05-30 23:29:57 +0300

    :rocket: updated the todo schema

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -2,21 +2,23 @@ import mongoose from "mongoose";
 import Todo from "../models/todo.js"
 
 export const getTask = async(req,res)=>{
-
+    //get specific task searching by id
 }
 
 export const updateTask = async(req,res)=>{
-
+    //update task with specific id
 }
 
 export const createTask = async(req,res)=>{
-
+    //create task. only get here when registered user is authorized
 }
 
 export const deleteTask = async(req,res)=>{
-
+    //delete task with specific id, only if authorized
 }
 
-export const getTasks = async(req,res)=>{
-
+export const getTasks = async(req,res)=>{   
+    //get all tasks from specific user. either /me or /:id 
+    // /me renders in a different location and only to authorized users
+    // /:id renders public only headlines
 }
--- server/models/todo.js
+++ server/models/todo.js
@@ -5,19 +5,35 @@ const todoSchema = mongoose.Schema({
     title:{
         type:String,
         required:true,
-        min: 6,
-        max: 20
+        min:6,
+        max:20
     },
     createdAt:{
         type:Date,
         default: new Date()
     },
     tasks:{
-        type:[String],
+        type:[
+            {type:String, id:uuid(), completed:false }
+        ],
         default:[]
     },
-    creator:String
-})
+    author:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    priority:{
+        type:Number,
+        required:true,
+        min:0,
+        max:4
+    },
+    privacy:{
+        type:String,
+        default:"Public"
+    }
+},
+{timestamps:true})
 
 const Todo = mongoose.model("Todos", todoSchema)
 
