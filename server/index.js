4a0c5ed6055758693e9481eb3e92e59e82f04bdb
Nick <tnikos28@gmail.com>
2023-05-30 13:12:27 +0300

    :rocket: server setup

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors';
+import dotenv from 'dotenv'
+
 
 dotenv.config();
 
 const app = express();
+app.use(bodyParser.json({limit:"30mb", extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
+app.use(cors());
 const PORT = process.env.PORT
 
 app.listen(PORT, ()=>
