4391d1ed6b5912e028fde6b21f3642baa3a45ae8
Nick <tnikos28@gmail.com>
2023-06-27 17:12:54 +0300

    added a typecheck for token

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,7 +5,7 @@ const secret = 'test-key';
 const auth = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-
+    console.log(typeof token)
     let decodedData;
 
     if (token) {      
