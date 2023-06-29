a339de8645f7e3b6010b4233f5c10ee67a661202
Nick <tnikos28@gmail.com>
2023-06-30 01:27:49 +0300

    changed parameters in pagination

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -1,13 +1,22 @@
 import Todo from "../models/todo.js";
 
 export const getTasksByPage = async (req,res) =>{
+    //get page from query
     const {page} = req.query;
-    console.log('eeee')
+    //if page is null or a number smaller than 0, return 400 code.
+    if(!Number(page) || Number(page) <= 0) return res.status(400).json({msg:'invalid parameter'})
     try{
+        //hardcoded defined limit, normally set within a .env
         const LIMIT = 12;
+        //define start and get total number of items
         const startIndex = (Number(page) - 1) * LIMIT; 
         const total = await Todo.find({completed:false, privacy:'Public'}).countDocuments({});
+        //if page number is bigger than the maxNum of pages return 400 code
+        if(Math.ceil(total/LIMIT) < page) return res.status(404).json({msg:'invalid parameter'})
+
+        //fetch the tasks requested, set limit ,sort and skip depending on starting index
         const tasks = await Todo.find({completed:false, privacy:'Public'}).sort({createdAt:-1}).limit(LIMIT).skip(startIndex);
+        //return tasks, current page, number of pages
         res.status(200).json({data: tasks, currentPage: Number(page), numberOfPages: Math.ceil(total/LIMIT)})
     }
     catch(err){
@@ -15,3 +24,4 @@ export const getTasksByPage = async (req,res) =>{
     }
 }
 //LOGIC Sget index. if page = 1, (1-1) * 8 = 0. starting at 0. if page = 2 , (2-1) * 8 = 8   
+//NUM PAGES => if limit = 12 and total 55, 55/12 =  4.583 => num_pages = 5, which basicaly means, 4*12=48, 55-48 = 7 , 4 full pages and 1 5th page with 7/12 limit
