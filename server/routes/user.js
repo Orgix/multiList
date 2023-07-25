d8366377db13693228d810aba9a8b180e79f8d2b
Nick <tnikos28@gmail.com>
2023-07-25 21:49:53 +0300

    added new backend route to handle incoming toggling for favorite status

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { deleteUser, loginUser, registerUser, signout, updateUser } from "../controllers/user.js";
+import { deleteUser, loginUser, registerUser, signout, updateUser,toggleFavorite } from "../controllers/user.js";
 import { validateUser } from "../middleware/dataValidation.js";
 
 const router = express.Router();
@@ -10,4 +10,5 @@ router.post('/signin', validateUser, loginUser)
 router.post('/logout', signout)
 router.patch('/update', updateUser)
 router.delete('/delete/:userId', deleteUser)
+router.patch('/favorites/:taskId', toggleFavorite)
 export default router;
