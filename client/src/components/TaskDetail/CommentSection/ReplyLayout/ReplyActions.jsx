64329c0d5db3fd391f8d98d57c75cd7d3e4e19c6
Nick <tnikos28@gmail.com>
2023-07-28 15:24:31 +0300

    completed majority of the reply feature

--- client/src/components/TaskDetail/CommentSection/ReplyLayout/ReplyActions.jsx
+++ client/src/components/TaskDetail/CommentSection/ReplyLayout/ReplyActions.jsx
@@ -1,69 +1,69 @@
 import { useState } from 'react'
 import { Button,Typography, Paper, TextField, Container, Box, IconButton,Tooltip, Menu,MenuItem,Link } from '@mui/material'
-import SendIcon from '@mui/icons-material/Send';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import {Link as RouterLink} from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux'
+import {Link as RouterLink, useParams} from 'react-router-dom'
+import { fetchReplies,postReply, deleteSuggestion } from '../../../../services/actions/tasks'
+import { getComment } from '../../../../services/state/commentSlice'
+import Comment from '../Comment'
 import { styles } from './styles'
+import { getTaskById } from '../../../../services/state/taskSlice'
 
-const ReplyActions = () => {
+const ReplyActions = ({suggestionId, replies}) => { 
+  
   const [openReply, setOpenReply] = useState(false)
   const [openReplies, setOpenReplies] = useState(false)
+  const [replyData, setReplyData] = useState('')
+  const dispatch = useDispatch()
+  const {id} = useParams();
+  const user = useSelector((state)=> state.auth.user)
+  const author = useSelector((state)=> getTaskById(state,id)).author
+
+  const handleChange = (event) =>{
+    setReplyData(event.target.value)
+  }
+
+  const handleReply = () =>{
+    //if not empty proceed
+    if(replyData.trim() !== ''){
+      const newSuggestion = {
+        text:replyData,
+        author:{
+          name:`${user.firstName} ${user.lastName}`,
+          authorID: user.id
+        },
+        isReply:true,
+        to:suggestionId
+      }
+      dispatch(postReply({id : suggestionId, reply:  newSuggestion}))
+      setReplyData('')
+    }
+  }
+  
+  const deleteComment = (suggestionId) =>{
+    dispatch(deleteSuggestion({taskId: taskID, suggestionId: suggestionId}))
+  }
   
   const handleClickReply = () =>{
     setOpenReply(true)
   }
+
   const handleClickReplies = () =>{
+    dispatch(fetchReplies(suggestionId))
     setOpenReplies(!openReplies)
   }
+  const replyObjs = useSelector((state)=> getComment(state,suggestionId)).replies
   return (
     <>
         <Button sx={{position:'absolute',top:'60px', left:'13px'}} onClick={handleClickReply} variant="text" >Reply</Button>
-        <Button sx={{position:'absolute',top:'60px', left:'66px'}} onClick={handleClickReplies} variant="text" >{!openReplies ? 'View replies(4)' : 'Hide Replies'}</Button>
+        {replies > 0 && <Button sx={{position:'absolute',top:'60px', left:'66px'}} onClick={handleClickReplies} variant="text" >{!openReplies ? `View replies (${replies})` : 'Hide Replies'}</Button>}
         {openReplies && 
             <>  
                 <Container sx={{mt:4}} disableGutters>
-            
-                <Paper key="1" elevation={3} sx={{ml:3, my:1, p:1}}>
-                    <Box sx={{display:'flex'}}> 
-                        <Link component={RouterLink} underline="none"><b>uwunistis</b></Link> 
-                        <Typography variant="subtitle1">&nbsp;- 5 minutes ago</Typography> 
-                        <Typography>(edited)</Typography>
-                    </Box>
-                    <Typography>  
-                        reply text 1
-                    </Typography>
-                </Paper>
-                <Paper key="3" elevation={3} sx={{ml:3, my:1, p:1}}>
-                    <Box sx={{display:'flex'}}> 
-                        <Link component={RouterLink} underline="none"><b>uwunistis</b></Link> 
-                        <Typography variant="subtitle1">&nbsp;- 5 minutes ago</Typography> 
-                        <Typography>(edited)</Typography>
-                    </Box>
-                    <Typography>  
-                        reply text 1
-                    </Typography>
-                </Paper>
-                <Paper key="4" elevation={3} sx={{ml:3, my:1, p:1}}>
-                    <Box sx={{display:'flex'}}> 
-                        <Link component={RouterLink} underline="none"><b>uwunistis</b></Link> 
-                        <Typography variant="subtitle1">&nbsp;- 5 minutes ago</Typography> 
-                        <Typography>(edited)</Typography>
-                    </Box>
-                    <Typography>  
-                        reply text 1
-                    </Typography>
-                </Paper>
-                <Paper key="2" elevation={3} sx={{ml:3, my:1, p:1}}>
-                    <Box sx={{display:'flex'}}> 
-                        <Link component={RouterLink} underline="none"><b>uwunistis</b></Link> 
-                        <Typography variant="subtitle1">&nbsp;- 5 minutes ago</Typography> 
-                        <Typography>(edited)</Typography>
-                    </Box>
-                    <Typography>  
-                        reply text 1
-                    </Typography>
-                </Paper>
-                
+                {Array.isArray(replyObjs) ? 
+                    replyObjs.map(reply=>{
+                        return <Comment key={reply._id} comment={reply} deleteComment={deleteComment} authorAccess={user.id === author.authorID} suggestionAuthorAccess={user.id === reply.author.authorID} />
+                    }): '' 
+                }
                 </Container>
                 
             </>
@@ -72,8 +72,8 @@ const ReplyActions = () => {
             <>
                 <Box sx={{my:4, ml:4}}>
                     <Container sx={styles.Editcontainer} disableGutters>
-                        <TextField variant="outlined" sx={styles.input} InputProps={{ sx: { borderTopRightRadius: 0, borderBottomRightRadius:0 } }} type="text" label="Post a reply" name="reply"/>
-                        <Button size="large" sx={styles.saveEditbtn}>Save</Button>
+                        <TextField variant="outlined" sx={styles.input} InputProps={{ sx: { borderTopRightRadius: 0, borderBottomRightRadius:0 } }} value={replyData} onChange={handleChange} type="text" label="Post a reply" name="reply"/>
+                        <Button size="large" onClick={handleReply} sx={styles.saveEditbtn}>Save</Button>
                     </Container>
                 </Box>
             </>
