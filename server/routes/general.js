93abc6c0eed9934698e477e09aa2c310e4291db4
Nick <tnikos28@gmail.com>
2023-07-28 15:28:32 +0300

    added new routes for replies

--- server/routes/general.js
+++ server/routes/general.js
@@ -1,10 +1,12 @@
 import express from  'express'
-import { getTasksByPage, getTaskSuggestions,postSuggestion,deleteSuggestion, editSuggestion } from '../controllers/general.js';
+import { getTasksByPage, getTaskSuggestions,postSuggestion,deleteSuggestion, editSuggestion,fetchReplies,postReply } from '../controllers/general.js';
 
 const router = express.Router();
 
 router.get('/', getTasksByPage)
 router.get('/:taskId/suggestions', getTaskSuggestions)
+router.get('/suggestions/:suggestionId/replies',fetchReplies)
+router.post('/suggestions/:suggestionId/replies', postReply)
 router.post('/:taskId/suggestions', postSuggestion)
 router.delete('/:taskId/suggestions/:suggestionId', deleteSuggestion)
 router.patch('/suggestions/:suggestionId', editSuggestion)
