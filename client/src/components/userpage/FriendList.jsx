b686244dceb5ed258d499a70d0eed470cc0d17ec
Nick <tnikos28@gmail.com>
2023-08-15 10:07:49 +0300

    setup for deleting friend from friend List component

--- client/src/components/userpage/FriendList.jsx
+++ client/src/components/userpage/FriendList.jsx
@@ -1,11 +1,17 @@
 import { Box, Container, Paper, Typography,Tooltip,Button } from '@mui/material';
-import React from 'react'
+import {useState} from 'react'
 import DoneIcon from '@mui/icons-material/Done';
 import ClearIcon from '@mui/icons-material/Clear';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import { useDispatch, useSelector } from 'react-redux'
+import DialogWindow from '../TaskDetail/DialogWindow';
+
 const FriendList = () => {
-  const friends = useSelector((state)=>state.auth.user.friends);
-  
+    const [open,setOpen] = useState(false)
+    const friends = useSelector((state)=>state.auth.user.friends);
+  const  deleteFriend = (userId) =>{
+    setOpen(false)
+  }
   return (
     <Container>
         <Box>
@@ -13,14 +19,15 @@ const FriendList = () => {
         </Box>
         {friends.map(friend=>{
             return (
-            <Paper sx={{py:2, my:2,display:'flex',position:'relative'}}>
+            <Paper key={friend._id} sx={{py:2, my:2,display:'flex',position:'relative'}}>
                 <Typography sx={{ml:1}}>{friend.firstName}</Typography>-<Typography>{friend.lastName}</Typography>
                 <Box sx={{position:'absolute', right:'5px',bottom:'9px'}}>
                     <Tooltip arrow title="Deny Request">
-                        <Button variant="contained" color="error" sx={{marginLeft:'3px'}}>
+                        <Button variant="contained" sx={{marginLeft:'3px'}} onClick={()=>setOpen(true)}>
                             <ClearIcon/>
                         </Button>
                     </Tooltip>
+                    <DialogWindow open={open} setOpen={setOpen} confirm={()=>deleteFriend(friend._id)} title="Removing user from friend List does not remove him/her from a team that the user is already part of. It will however disable inviting the user to any additional tasks."/>
                 </Box>
             </Paper>
         )
