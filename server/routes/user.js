c8898b4bf200491c9736c577f808d5198b2c7c20
Nick <tnikos28@gmail.com>
2023-08-29 12:45:25 +0300

    added new route for resolving requests

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { deleteUser, loginUser, registerUser, signout, updateUser,toggleFavorite, addUserToFriendList, deleteUserFromFriendList, cancelRequest } from "../controllers/user.js";
+import { deleteUser, loginUser, registerUser, signout, updateUser,toggleFavorite, addUserToFriendList, deleteUserFromFriendList, cancelRequest,resolveRequest } from "../controllers/user.js";
 import { validateUser } from "../middleware/dataValidation.js";
 
 const router = express.Router();
@@ -11,6 +11,7 @@ router.post('/logout', signout)
 router.patch('/update', updateUser)
 router.delete('/delete/:userId', deleteUser)
 router.delete('/:userId/requests/cancel/:requestId',cancelRequest)
+router.put('/requests/:requestId/resolve/:response', resolveRequest)
 router.post('/add/:userId', addUserToFriendList)
 router.patch('/delete/:userId', deleteUserFromFriendList)
 router.patch('/favorites/:taskId', toggleFavorite)
@@ -18,3 +19,4 @@ router.patch('/favorites/:taskId', toggleFavorite)
 export default router;
 
 // /auth/${userId}/requests/cancel/${requestId}
+// /auth/requests/${requestId}/resolve/${response}
