751feae0a5112813e68e7ff4f6aa44ae6c0e6858
Nick <tnikos28@gmail.com>
2023-09-01 12:23:05 +0300

    :rocket: added an authorization middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,23 +1,18 @@
 import jwt from "jsonwebtoken";
 
-const secret = 'test-key';
+//this controller is used as middleware to determine if the user requesting a change/addition/deletion
+//has any level of authorization
+export const authorizeUser = (req,res,next) =>{
+  //determine if there is any token 
+  const header = req.headers.authorization
+  //if undefined, deny access
+  if(!header) return res.status(403).json({msg:'Unauthorized'})
 
-const auth = async (req, res, next) => {
-  try {
-    const token = req.headers.authorization.split(" ")[1];
-    console.log(typeof token)
-    let decodedData;
-
-    if (token) {      
-      decodedData = jwt.verify(token, secret);
-
-      req.userId = decodedData?.id;
-    } 
-
-    next();
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export default auth;
+  //get token and decode it
+  const token = req.headers.authorization.split(' ')[1]
+  const decoded = jwt.decode(token, process.env.JWT_SECRET)
+  
+  req.token = token
+  req.decoded = decoded;
+  next();
+}
