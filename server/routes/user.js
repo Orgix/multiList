ac6da90d6c3cf75b6a584e75ded76bc87d46708b
Nick <tnikos28@gmail.com>
2023-09-06 13:50:40 +0300

    :alien: added fetching requests route

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { deleteUser, loginUser, registerUser, signout, updateUser,toggleFavorite, addUserToFriendList, deleteUserFromFriendList, cancelRequest,resolveRequest } from "../controllers/user.js";
+import { deleteUser, loginUser, registerUser, signout, updateUser,toggleFavorite, addUserToFriendList, deleteUserFromFriendList, cancelRequest,resolveRequest, fetchRequests } from "../controllers/user.js";
 import { validateUser } from "../middleware/dataValidation.js";
 import { authorizeUser } from "../middleware/auth.js";
 
@@ -13,6 +13,7 @@ router.patch('/update', authorizeUser, updateUser)
 router.delete('/delete/:userId', authorizeUser, deleteUser)
 router.delete('/:userId/requests/cancel/:requestId', authorizeUser, cancelRequest)
 router.put('/requests/:requestId/resolve/:response', authorizeUser, resolveRequest)
+router.get('/requests/fetch',authorizeUser, fetchRequests)
 router.post('/add/:userId', authorizeUser, addUserToFriendList)
 router.patch('/delete/:userId', authorizeUser, deleteUserFromFriendList)
 router.patch('/favorites/:taskId', authorizeUser, toggleFavorite)
