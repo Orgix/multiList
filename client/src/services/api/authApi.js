28c358cb25c739ac4774fbf0bccba0d31a054861
Nick <tnikos28@gmail.com>
2023-09-07 11:05:58 +0300

    added auth axios actions file

---
+++ client/src/services/api/authApi.js
@@ -0,0 +1,5 @@
+import API from './apiConfig'; // Import your Axios instance
+
+export const signIn = (formData) => API.post('/auth/signin', formData);
+export const signUp = (formData) => API.post('/auth/signup', formData);
+export const signOut = () => API.post('/auth/logout');
