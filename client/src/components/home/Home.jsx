b0aec8414aa472e9cb7ee8a5e772c4709a0983a7
Nick <tnikos28@gmail.com>
2023-09-26 13:03:18 +0300

    refactor component

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,9 +1,7 @@
-import React, { useEffect } from 'react'
-import {useSelector} from 'react-redux'
-import { getTasks,getTasksStatus,getTasksError } from '../../services/state/taskSlice'
-import { Container, Grid } from '@mui/material'
-import { fetchTasks } from '../../services/api';
-import TaskBar from '../sidenav/TaskBar'
+import React from 'react'
+
+import {  Grid } from '@mui/material'
+
 import Tasks from '../Tasks/Tasks'
 
 const Home = () => {
