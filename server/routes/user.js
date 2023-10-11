b0bc4e3c00e4f4b8b162062ff2aa106d3e37cc6e
Nick <tnikos28@gmail.com>
2023-10-11 13:08:01 +0300

    added new route for task invites

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,12 @@
 import express from "express";
 
-import { deleteUser, loginUser, registerUser, signout, updateUser,toggleFavorite, addUserToFriendList, deleteUserFromFriendList, cancelRequest,resolveRequest, fetchRequests, searchUser,inviteUsersToTask } from "../controllers/user.js";
+import { deleteUser, loginUser, registerUser, signout, updateUser,toggleFavorite, addUserToFriendList, deleteUserFromFriendList, cancelRequest,resolveRequest, fetchRequests, searchUser,inviteUsersToTask,resolveTaskInvite } from "../controllers/user.js";
 import { validateUser } from "../middleware/dataValidation.js";
 import { authorizeUser,validateAuthor } from "../middleware/auth.js";
 
 const router = express.Router();
 
+//all routes have a protection, they can't be used unless the user is validated or authorized 
 router.post('/signup', validateUser, registerUser);
 router.post('/signin', validateUser, loginUser)
 router.post('/logout', authorizeUser, signout)
@@ -13,6 +14,7 @@ router.patch('/update', authorizeUser, updateUser)
 router.delete('/delete/:userId', authorizeUser, deleteUser)
 router.delete('/:userId/requests/cancel/:requestId', authorizeUser, cancelRequest)
 router.put('/requests/:requestId/resolve/:response', authorizeUser, resolveRequest)
+router.put('/requests/:requestId/taskInvite/resolve/:response', authorizeUser, resolveTaskInvite)
 router.get('/requests/fetch',authorizeUser, fetchRequests)
 router.post('/add/:userId', authorizeUser, addUserToFriendList)
 router.patch('/delete/:userId', authorizeUser, deleteUserFromFriendList)
